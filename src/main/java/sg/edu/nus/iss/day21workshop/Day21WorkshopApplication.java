ad9bd88443c7314ab9a337569e68d71ff7465ceb
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-20 22:18:33 +0800

    initial commit with new db user created

---
+++ src/main/java/sg/edu/nus/iss/day21workshop/Day21WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day21workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day21workshop/Day21WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day21workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
